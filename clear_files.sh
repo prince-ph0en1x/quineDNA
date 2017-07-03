ee4dab7d57cca89d8e28a966508073e22e490e31
prince-ph0en1x <aritra44sarkar@yahoo.co.in>
2017-07-03 12:42:28 +0530

    v6 added cell auto death

--- clear_files.sh
+++ clear_files.sh
@@ -3,4 +3,3 @@
    do
       rm "$file"
    done
-   
--- quine_v6.java
+++ quine_v6.java
@@ -29,6 +29,13 @@ class quine		// Initial female zygote that creates two individuals of opposite s
 	final static String maleInit =  "219A83C8D888F8FVC8V8D81A91ABDF83FE028F65";
 	public static void main(String args[])
 	{
+		try {
+			ProcessBuilder pb = new ProcessBuilder("rm","quine_v6_G"+gen+"_O"+orgmID+"T"+typeID+".java");	// Kill source cell
+			Process p = pb.start();
+			p.waitFor();
+		}
+		catch (Exception e) {
+		}
 		String malePheno = "";
 		if(typeID == 3) {
 			if (orgmID%2 == 0) {	// if male zygote, return phenotype for crossover by female
@@ -39,17 +46,15 @@ class quine		// Initial female zygote that creates two individuals of opposite s
 				malePheno = args[0];
 			}
 		}
-		
 		BufferedWriter bw = null;
 		for(int type=0;type<size[typeID].length;type++) {
 			for (int nos=0;nos<size[typeID][type];nos++) {
-				try {
-		   			int oi = (orgmID-1)*size[typeID][type]+nos+1;		// Organism ID of new generation
-		   			String cn = type != 3 ? "" : (oi%2 == 0 ? "M" : "F");	// Class Name : genderless/female/male
-		   			int gi = type == 0 ? (gen+1) : gen;			// Generation ID of new generation
-		   			String op = oi == 1 ? pheno : maleInit;			// Offspring Phenotype is otherwise : typeID == 3 ? cross(malePheno,oi) : pheno;
-		   			String DNA = "import java.io.*;\nclass quine"+cn+"\n{\n\tfinal static int gen = "+gi+";\n\tfinal static int orgmID = "+oi+";\n\tfinal static int typeID = "+type+";\n\tfinal static int size[][] = {{0,1,1,1},{0,0,0,0},{0,0,0,0},{2,0,0,0}};\n\tfinal static String pheno = \""+op+"\";\n\tpublic static void main(String args[])\n\t{\n\t\tString malePheno = \"\";\n\t\tif(typeID == 3) {\n\t\t\tif (orgmID%2 == 0) {\n\t\t\t\tSystem.out.println(pheno);\n\t\t\t\treturn;\n\t\t\t}\n\t\t\telse {\n\t\t\t\tmalePheno = args[0];\n\t\t\t}\n\t\t}\n\t\tBufferedWriter bw = null;\n\t\tfor(int type=0;type<size[typeID].length;type++) {\n\t\t\tfor (int nos=0;nos<size[typeID][type];nos++) {\n\t\t\t\ttry {\n\t\t\t\t\tint oi = (orgmID-1)*size[typeID][type]+nos+1;\n\t\t\t\t\tString cn = type != 3 ? \"\" : (oi%2 == 0 ? \"M\" : \"F\");\n\t\t\t\t\tint gi = type == 0 ? (gen+1) : gen;\n\t\t\t\t\tString op = typeID == 3 ? cross(malePheno,oi) : pheno;\n\t\t\t\t\tString DNA = @;\n\n\t\t\t\t\tbw = new BufferedWriter(new FileWriter(\"quine_v6_G\"+(type == 0 ? (gen+1) : gen)+\"_O\"+((orgmID-1)*size[typeID][type]+nos+1)+\"T\"+type+\".java\"));\n\t\t\t\t\tbw.write(process(DNA,cn,gi,oi,type,op));\n\t\t\t\t\tbw.flush();\n\t\t\t\t\tbw.close();\n\t\t\t\t}\n\t\t\t\tcatch (Exception e) {\n\t\t\t\t}\n\t\t\t}\n\t\t}\n\t}\n\tpublic static String process(String baseDNA,String cn,int gi,int oi,int type,String op)\n\t{\n\t\tString bs = baseDNA.replaceAll(\"\\\\\\\\\", \"\\\\\\\\\\\\\\\\\");\n\t\tString bn = bs.replaceAll(\"\\n\", \"\\\\\\\\n\");\n\t\tString bt = bn.replaceAll(\"\\t\", \"\\\\\\\\t\");\n\t\tString dq = bt.replaceAll(\"\\\"\", \"\\\\\\\\\\\"\");\n\t\tString cl = dq.replaceFirst(\"quine\"+cn, \"quine\\\"+cn+\\\"\");\n\t\tString g = cl.replaceFirst(\"gen = \"+gi, \"gen = \\\"+gi+\\\"\");\n\t\tString c = g.replaceFirst(\"orgmID = \"+oi, \"orgmID = \\\"+oi+\\\"\");\n\t\tString t = c.replaceFirst(\"typeID = \"+type, \"typeID = \\\"+type+\\\"\");\n\t\tString cr = t.replaceFirst(op, \"\\\"+op+\\\"\");\n\t\tString newDNA = \"\";\n\t\tfor (int i=0;i<baseDNA.length();i++)\n\t\t\tif (baseDNA.charAt(i) == 64)\n\t\t\t\tnewDNA = baseDNA.substring(0,i)+\"\\\"\"+cr+\"\\\"\"+baseDNA.substring(i+1);\n\t\treturn newDNA;\n\t}\n\tpublic static String cross(String phenoMate,int oi)\n\t{\n\t\tint cp = (int)(Math.random()*pheno.length());\n\t\tif (oi%2 == 0)\n\t\t\treturn pheno.substring(0,cp)+phenoMate.substring(cp);\n\t\telse\n\t\t\treturn phenoMate.substring(0,cp)+pheno.substring(cp);\n\t}\n}";
-					
+				int oi = (orgmID-1)*size[typeID][type]+nos+1;		// Organism ID of new generation
+		   		String cn = type != 3 ? "" : (oi%2 == 0 ? "M" : "F");	// Class Name : genderless/female/male
+		   		int gi = type == 0 ? (gen+1) : gen;			// Generation ID of new generation
+		   		String op = oi == 1 ? pheno : maleInit;			// Offspring Phenotype is otherwise : typeID == 3 ? cross(malePheno,oi) : pheno;
+		   		String DNA = "import java.io.*;\nclass quine"+cn+"\n{\n\tfinal static int gen = "+gi+";\n\tfinal static int orgmID = "+oi+";\n\tfinal static int typeID = "+type+";\n\tfinal static int size[][] = {{0,1,1,1},{0,0,0,0},{0,0,0,0},{2,0,0,0}};\n\tfinal static String pheno = \""+op+"\";\n\tpublic static void main(String args[])\n\t{\n\t\ttry {\n\t\t\tProcessBuilder pb = new ProcessBuilder(\"rm\",\"quine_v6_G\"+gen+\"_O\"+orgmID+\"T\"+typeID+\".java\");\n\t\t\tProcess p = pb.start();\n\t\t\tp.waitFor();\n\t\t}\n\t\tcatch (Exception e) {\n\t\t}\n\t\tString malePheno = \"\";\n\t\tif(typeID == 3) {\n\t\t\tif (orgmID%2 == 0) {\n\t\t\t\tSystem.out.println(pheno);\n\t\t\t\treturn;\n\t\t\t}\n\t\t\telse {\n\t\t\t\tmalePheno = args[0];\n\t\t\t}\n\t\t}\n\t\tBufferedWriter bw = null;\n\t\tfor(int type=0;type<size[typeID].length;type++) {\n\t\t\tfor (int nos=0;nos<size[typeID][type];nos++) {\n\t\t\t\tint oi = (orgmID-1)*size[typeID][type]+nos+1;\n\t\t\t\tString cn = type != 3 ? \"\" : (oi%2 == 0 ? \"M\" : \"F\");\n\t\t\t\tint gi = type == 0 ? (gen+1) : gen;\n\t\t\t\tString op = typeID == 3 ? cross(malePheno,oi) : pheno;\n\t\t\t\tString DNA = @;\n\t\t\t\ttry {\n\t\t\t\t\tbw = new BufferedWriter(new FileWriter(\"quine_v6_G\"+(type == 0 ? (gen+1) : gen)+\"_O\"+((orgmID-1)*size[typeID][type]+nos+1)+\"T\"+type+\".java\"));\n\t\t\t\t\tbw.write(process(DNA,cn,gi,oi,type,op));\n\t\t\t\t\tbw.flush();\n\t\t\t\t\tbw.close();\n\t\t\t\t}\n\t\t\t\tcatch (Exception e) {\n\t\t\t\t}\n\t\t\t}\n\t\t}\n\t\t\n\t}\n\tpublic static String process(String baseDNA,String cn,int gi,int oi,int type,String op)\n\t{\n\t\tString bs = baseDNA.replaceAll(\"\\\\\\\\\", \"\\\\\\\\\\\\\\\\\");\n\t\tString bn = bs.replaceAll(\"\\n\", \"\\\\\\\\n\");\n\t\tString bt = bn.replaceAll(\"\\t\", \"\\\\\\\\t\");\n\t\tString dq = bt.replaceAll(\"\\\"\", \"\\\\\\\\\\\"\");\n\t\tString cl = dq.replaceFirst(\"quine\"+cn, \"quine\\\"+cn+\\\"\");\n\t\tString g = cl.replaceFirst(\"gen = \"+gi, \"gen = \\\"+gi+\\\"\");\n\t\tString c = g.replaceFirst(\"orgmID = \"+oi, \"orgmID = \\\"+oi+\\\"\");\n\t\tString t = c.replaceFirst(\"typeID = \"+type, \"typeID = \\\"+type+\\\"\");\n\t\tString cr = t.replaceFirst(op, \"\\\"+op+\\\"\");\n\t\tString newDNA = \"\";\n\t\tfor (int i=0;i<baseDNA.length();i++)\n\t\t\tif (baseDNA.charAt(i) == 64)\n\t\t\t\tnewDNA = baseDNA.substring(0,i)+\"\\\"\"+cr+\"\\\"\"+baseDNA.substring(i+1);\n\t\treturn newDNA;\n\t}\n\tpublic static String cross(String phenoMate,int oi)\n\t{\n\t\tint cp = (int)(Math.random()*pheno.length());\n\t\tif (oi%2 == 0)\n\t\t\treturn pheno.substring(0,cp)+phenoMate.substring(cp);\n\t\telse\n\t\t\treturn phenoMate.substring(0,cp)+pheno.substring(cp);\n\t}\n}";
+				try {	
 					bw = new BufferedWriter(new FileWriter("quine_v6_G"+(type == 0 ? (gen+1) : gen)+"_O"+((orgmID-1)*size[typeID][type]+nos+1)+"T"+type+".java"));
 		   			bw.write(process(DNA,cn,gi,oi,type,op));
 		   			bw.flush();
@@ -58,7 +63,7 @@ class quine		// Initial female zygote that creates two individuals of opposite s
 		   		catch (Exception e) {
 				}
 			}
-		}
+		}	
 	}
 	public static String process(String baseDNA,String cn,int gi,int oi,int type,String op)
 	{
